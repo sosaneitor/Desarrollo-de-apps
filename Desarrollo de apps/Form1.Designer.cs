08f06d98b8bb85fa0c8d21aae32ba0369ff81295
amdr0 <amdr0@POISON>
2023-03-11 16:50:14 -0500

    Agregar archivos de proyecto.

---
+++ Desarrollo de apps/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Desarrollo_de_apps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textCedula = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Agregar ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(100, 46);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(117, 20);
+            this.textNombre.TabIndex = 1;
+            // 
+            // textCedula
+            // 
+            this.textCedula.Location = new System.Drawing.Point(100, 86);
+            this.textCedula.Name = "textCedula";
+            this.textCedula.Size = new System.Drawing.Size(117, 20);
+            this.textCedula.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " NOMBRE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " CEDULA";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = " Mostras Agregados";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 132);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textCedula);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textCedula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Desarrollo de apps/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Desarrollo_de_apps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //se crea el diccionario 
+        private Dictionary<string, string> miDiccionario = new Dictionary<string, string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Capturar el nombre y la cédula del usuario
+            string nombre = textNombre.Text;
+            string cedula = textCedula.Text;
+
+            // Validar que los campos no estén vacíos
+            if (nombre != "" && cedula != "")
+            {
+                // Agregar los datos al diccionario
+                miDiccionario.Add(nombre, cedula);
+
+                // Limpiar los campos de texto
+                textNombre.Text = "";
+                textCedula.Text = "";
+
+                // Mostrar un mensaje de éxito
+                MessageBox.Show("Datos guardados correctamente.");
+            }
+            else
+            {
+                // Mostrar un mensaje de error si algún campo está vacío
+                MessageBox.Show("Por favor ingrese el nombre y la cédula.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Recorrer el diccionario y mostrar los datos
+            foreach (KeyValuePair<string, string> persona in miDiccionario)
+            {
+                MessageBox.Show($"Nombre: {persona.Key}\nCédula: {persona.Value}");
+            }
+        }
+    }
+}
